e76a61bd6fbb27c18f954527c1f42cbd0d316cbf
Noah <noah.nlandu.333@gmail.com>
2018-06-18 13:26:10 -0700

    New Form
    Hope this works

--- submit.php
+++ submit.php
@@ -1,26 +1,35 @@
 <?php
-// if the url field is empty
-if(isset($_POST['url']) && $_POST['url'] == ''){
-	// then send the form to your email
-	mail( 'me@noahnlandu.com', 'Noah Nlandu', print_r($_POST,true) );
-}
-// otherwise, let the spammer think that they got their message through
+     if(isset($_POST['message']) == false) {     //  If there's no message
+        echo "Uh oh. Looks like you didn't actually include a message, friend.<br><br>";
+        die();   
+    }
+    
+    
+    $destination = "me@noahnlandu.com";       //  Put your email address here
+    $subject = "Message from your baller website!";   //  Fill in the subject line you want your messages to have
+    $fromAddress = "example@example.com";   //  Fill in the email address that you want the messages to appear to be from
+                                                                    //  Use a real address to reduce the odds of getting spam-filtered.
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = str_replace("\n.", "\n..", $_POST['message']);   //  Prevents a new line starting with a period from being omitted
+    $message = "Name: ". $name ."\nEmail: ". $email . "\nMessage: ".$message."\n";
+$headers   = array();
+$headers[] = "MIME-Version: 1.0";
+$headers[] = "Content-type: text/plain; charset=iso-8859-1";
+$headers[] = "From: " . $fromAddress;
+$headers[] = "Subject: " . $subject;
+$headers[] = "X-Mailer: PHP/".phpversion();
+mail($destination, $subject, $message, implode("\r\n", $headers));
+ 
+?>
 
+<p>It worked! Thanks for your message:<br>
+<?php echo $message; ?>
+ 
+<br>
+<a href="http://noahnlandu.com">Go back home</a>
+
+<!--
 header('Location: http://www.noahnlandu.com');
 exit('Redirecting you to http://www.noahnlandu.com');
-?>
-<!DOCTYPE HTML>
-<html>
-<head>
-
-<title>Thanks!</title>
-
-</head>
-<body>
-
-<h1>Thanks</h1>
-<p>We'll get back to you as soon as possible.</p>
-
-<body>
-</body>
-</html>
+-->
