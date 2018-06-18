90807aa61003cefe80346101f4c5d1b6d0f037f8
Noah <noah.nlandu.333@gmail.com>
2018-06-18 12:24:03 -0700

    Form Fix
    Last Attempt

--- submit.php
+++ submit.php
@@ -2,7 +2,7 @@
 // if the url field is empty
 if(isset($_POST['url']) && $_POST['url'] == ''){
 	// then send the form to your email
-	mail( 'you@yoursite.com', 'Contact Form', print_r($_POST,true) );
+	mail( 'me@noahnlandu.com', 'Noah Nlandu', print_r($_POST,true) );
 }
 // otherwise, let the spammer think that they got their message through
 ?>
